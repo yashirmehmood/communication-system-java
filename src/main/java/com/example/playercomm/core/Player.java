0f03a3911b8134ae8cb6e2a6f9327777749aa0bf
yashirmehmood <yashirbajwa1464@gmail.com>
2025-12-03 13:24:11 +0100

    First Commit - created PlayerFactory and Player

---
+++ src/main/java/com/example/playercomm/core/Player.java
@@ -0,0 +1,4 @@
+package com.example.playercomm.core;
+
+public class Player {
+}
---
+++ src/main/java/com/example/playercomm/factory/PlayerFactory.java
@@ -0,0 +1,10 @@
+package com.example.playercomm.factory;
+
+
+/**
+ * Factory responsible for creating Player instances.
+ * Encapsulates Player creation logic and allows future extensions.
+ */
+public class PlayerFactory {
+
+}
