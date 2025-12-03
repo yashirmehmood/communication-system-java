248679af69864628e0d7cb2a67b4eba19a4e1a35
yashirmehmood <yashirbajwa1464@gmail.com>
2025-12-03 17:56:12 +0100

    added the implementation of PlayerFactory

--- src/main/java/com/example/playercomm/factory/PlayerFactory.java
+++ src/main/java/com/example/playercomm/factory/PlayerFactory.java
@@ -1,10 +1,39 @@
 package com.example.playercomm.factory;
 
+import com.example.playercomm.core.Player;
+import com.example.playercomm.transport.MessageBroker;
 
 /**
- * Factory responsible for creating Player instances.
- * Encapsulates Player creation logic and allows future extensions.
+ * PlayerFactory is responsible for creating Player instances
+ * and registering them with a provided MessageBroker.
+ *
+ * Responsibilities:
+ * - Create new Player objects
+ * - Ensure they are registered with the broker immediately
  */
 public class PlayerFactory {
 
+    private final MessageBroker broker;
+
+    /**
+     * Constructs a PlayerFactory with the given MessageBroker.
+     *
+     * @param broker The MessageBroker to register players with
+     */
+    public PlayerFactory(MessageBroker broker) {
+        this.broker = broker;
+    }
+
+    /**
+     * Creates a new Player with the specified name, registers it with the broker,
+     * and returns the instance.
+     *
+     * @param playerName Unique name for the player
+     * @return Newly created Player instance
+     */
+    public Player createPlayer(String playerName) {
+        Player player = new Player(playerName, broker);
+        broker.registerPlayer(player);
+        return player;
+    }
 }
